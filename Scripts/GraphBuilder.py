33985df25da33e54ed6fa01732c19b9baaa3ac2f
RUI <jhcz@outlook.com>
2016-03-11 18:49:25 +0800

    3/11

--- Scripts/GraphBuilder.py
+++ Scripts/GraphBuilder.py
@@ -5,7 +5,7 @@ from progressbar import *
 import ipdb
 
 radius = 0.02
-weak_coef = 0.2
+weak_coef = 0.3
 
 def createKDTree(n_pts, data):
     tripples = arrayToTrippleList(data.data)
@@ -18,39 +18,39 @@ def arrayToTrippleList(arr):
         res.append((arr[3*i], arr[3*i+1], arr[3*i+2]))
     return res
 
-data = importFile("../../maya cache/03074/hair_nRigidShape1.xml")
-# graph = createInitGraph(data)
-frames = data
-
 # @profile
-# def createInitGraph(frames):
-ptGraph = igraph.Graph()
-n_pts = frames[0].n_particle
-n_frames = len(frames)
-n_weak_thresh = n_frames * weak_coef
+def createInitGraph(frames):
+    n_pts = frames[0].n_particle
+    n_frames = len(frames)
 
-ptGraph.add_vertices(n_pts)
-ptGraph.es["weight"] = 1.0
+    edgeHash = {}
+    pbar = ProgressBar().start()
+    for i in range(n_frames):
+        kdt = createKDTree(n_pts, frames[i%5])
+        pairs = kdt.query_pairs(radius)
+        pbar.update(((i/(n_frames-1.0))*100))
+        if i == 0:
+            edgeHash = dict.fromkeys(pairs, 1)
+            continue
+        for pair in pairs:
+            edgeHash.setdefault(pair, 0)
+            edgeHash[pair] += 1
 
-edgeHash = {}
-pbar = ProgressBar().start()
-for i in range(n_frames*20):
-    kdt = createKDTree(n_pts, frames[i%5])
-    pairs = kdt.query_pairs(radius)
+    pbar.finish()
+    return edgeHash
 
-    # print "process frame %d, with %d edges..." % (i, len(pairs))
-    pbar.update(((i/(n_frames*20.0-1))*100))
+def filterEdges(edges, thresh):
+    trash = []
+    for key in edges:
+        if edges[key] < thresh:
+            trash.append(key)
 
-    if i == 0:
-        edgeHash = dict.fromkeys(pairs, 1)
-        continue
+    for key in trash:
+        del edges[key]
 
-    for pair in pairs:
-        edgeHash.setdefault(pair, 0)
-        edgeHash[pair] += 1
+    return edges
 
-pbar.finish()
-
-weakLinks = ptGraph.es.select(weight_lt = n_weak_thresh)
-ptGraph.delete_edges(weakLinks)
-gg = ptGraph
+data = importFile("../../maya cache/03074/hair_nRigidShape1.xml")
+graph = createInitGraph(data)
+n_weak_thresh = len(data) * weak_coef
+filterEdges(graph, n_weak_thresh)
